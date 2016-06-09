88e4f8a32c891d028fd3ba3547f92c486b03fa06
Sp4rkY <macohnedonalds@gmx.de>
2016-06-09 04:49:58 +0200

    changed version

--- @epochhive/addons/epoch_SPK_ESVP/settings.h
+++ @epochhive/addons/epoch_SPK_ESVP/settings.h
@@ -2,7 +2,7 @@
 	Title:			SPK_ESVP
 	Author: 		Sp4rkY [https://github.com/SPKcoding]
 	Description:	Extended Safezones with Vehicle Protection for Arma 3 Epoch
-	Version:		0.436
+	Version:		0.443
 	File:			settings.h (serverside config)
 _______________________________________________________________________________________________________________________________________________________________________________________________ */
 
