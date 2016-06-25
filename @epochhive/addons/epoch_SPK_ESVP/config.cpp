61dd4d174cbe80bd5b1574f967b06b428916cc2d
Sp4rkY <macohnedonalds@gmx.de>
2016-06-25 12:46:29 +0200

    v0.444

--- @epochhive/addons/epoch_SPK_ESVP/config.cpp
+++ @epochhive/addons/epoch_SPK_ESVP/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
 		requiredAddons[] = {"A3_epoch_server"};
 		fileName = "epoch_SPK_ESVP.pbo";
 		requiredVersion = 1.60;
-		version = "0.443";
+		version = "0.444";
 		author[]= {"Sp4rkY"};
 	};
 };
