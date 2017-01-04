6a0c96de5372400885327f94a69b141e2f3c022a
Sp4rkY <macohnedonalds@gmx.de>
2017-01-05 00:06:31 +0100

    class CfgNotifications

---
+++ mpmissions/epoch.Mapname/SPKcode/notes.h
@@ -0,0 +1,21 @@
+class CfgNotifications {
+	
+	//ESVP
+	class EnterSZ {
+		title = "SAFEZONE";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskicondone_ca.paa";
+		color[] = {0,0.85,0,1};
+		description = "%1";
+		duration = 7;
+		priority = 0;
+	};
+	class LeaveSZ {
+		title = "";
+		iconPicture = "\A3\ui_f\data\map\mapcontrol\taskiconfailed_ca.paa";
+		color[] = {0.9,0,0,1};
+		description = "%1";
+		duration = 5;
+		priority = 0;
+	};
+	
+};
